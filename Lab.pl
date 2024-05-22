f3fbec8a5111184ef667b4edecce4c83bffb7ab9
Bryan Corradino <zeromuseg7@gmail.com>
2024-05-23 01:03:07 +0200

    1.9 (WIP), 2.2, 3.2

--- Lab.pl
+++ Lab.pl
@@ -8,7 +8,7 @@ search_two(E, [_ | Xs]) :- search_two(E, Xs).
 size(L, S) :- size(L, 0, S).
 size([], N, N).
 size([X | Xs], N, S) :- N2 is N + 1, size(Xs, N2, S).
-% È completamente relazionale.
+% Fully relational.
 % 1.4
 sum(L, S) :- sum(L, 0, S).
 sum([], N, N).
@@ -26,6 +26,34 @@ split([X | Xs], N, L1Temp, L1, L2) :- NNew is N - 1, append(L1Temp, [X], L1New),
 % 1.7
 rotate([X | Xs], R) :- append(Xs, [X], R).
 % 1.8
-dice(X) :- dice(0, 6, X).
-dice(B, T, X) :- B =< T, X is B + 1.
-dice(B, T, X) :- XNext is B + 1, XNext < T, dice(XNext, T, X).
+die(X) :- die(1, 6, X).
+die(S, _, S).
+die(C, E, X) :- C < E, CNext is C + 1, die(CNext, E, X).
+% 1.9
+% Can't make it stop early.
+three_dice(N, L) :- three_dice(N, L, L).
+three_dice(0, L, L) :- !.
+three_dice(N, L, L) :-
+  NN is N - 1,
+  three_dice(NN, [D1, D2, D3], L),
+  die(D1), die(D2), die(D3).
+
+% 2.2
+dropFirst(X, [X | T], T) :- !.
+dropFirst(X, [H | Xs], [H | L]) :- dropFirst(X, Xs, L).
+
+dropLast(X, [H | Xs], [H | L]) :- dropLast(X, Xs, L), !.
+dropLast(X, [X | T], T).
+
+dropAll(_, [], []).
+dropAll(X, [X | T], L) :- dropAll(X, T, L), !.
+dropAll(X, [H | Xs], [H | L]) :- dropAll(X, Xs, L).
+
+% 3.1
+% fromList(+List, -Graph)
+fromList([_], []).
+fromList([H1, H2 | T], [e(H1, H2) | L]) :- fromList([H2 | T], L).
+% 3.2
+fromCircList([H | T], G) :- fromCircList(H, [H | T], G).
+fromCircList(F, [E], [e(E, F)]).
+fromCircList(F, [H1, H2 | T], [e(H1, H2) | L]) :- fromCircList(F, [H2 | T], L).
